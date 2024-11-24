7dad529110ced387d22658a6e17988080ac98fe8
Joa <mikkel_ander@hotmail.com>
2024-11-24 20:25:27 +0100

    Fix the styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ export default {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ['monospace', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
