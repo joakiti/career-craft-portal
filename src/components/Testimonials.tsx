5c4a8186a3b9ff56794bb9ada69d4686af3c8488
Joa <mikkel_ander@hotmail.com>
2024-11-27 10:10:13 +0100

    Remove your company here

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -67,7 +67,6 @@ const companies: CompanyLogoProps[] = [
     { src: "/img/mim_logo.png", alt: "Make It Matter", name: "Make It Matter" },
     { src: "/img/godream_logo.jpeg", alt: "GoDream", name: "GoDream" },
     { src: "/img/itu_logo.png", alt: "ITU", name: "IT-University of Copenhagen" },
-    { src: "/img/placeholder_company.svg", alt: "You", name: "Your company here?" },
 ];
 
 const CompanyLogos = () => (
